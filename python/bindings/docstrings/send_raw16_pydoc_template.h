c83ed86e5248556fe1d829941bbbf5f38019eb60
Daniel Estévez <daniel@destevez.net>
2022-05-28 18:22:18 +0200

    Add missing files

---
+++ python/bindings/docstrings/send_raw16_pydoc_template.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 Free Software Foundation, Inc.
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+#include "pydoc_macros.h"
+#define D(...) DOC(gr,linrad, __VA_ARGS__ )
+/*
+  This file contains placeholders for docstrings for the Python bindings.
+  Do not edit! These were automatically extracted during the binding process
+  and will be overwritten during the build process
+ */
+
+
+ 
+ static const char *__doc_gr_linrad_send_raw16 = R"doc()doc";
+
+
+ static const char *__doc_gr_linrad_send_raw16_send_raw16_0 = R"doc()doc";
+
+
+ static const char *__doc_gr_linrad_send_raw16_send_raw16_1 = R"doc()doc";
+
+
+ static const char *__doc_gr_linrad_send_raw16_make = R"doc()doc";
+
+
+ static const char *__doc_gr_linrad_send_raw16_frequency = R"doc()doc";
+
+
+ static const char *__doc_gr_linrad_send_raw16_set_frequency = R"doc()doc";
+
+  
